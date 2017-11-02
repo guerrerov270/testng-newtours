dbeedea0be8626ff7accb5ed50fa98359722233c
guerrerov270 <jguerrero089@gmail.com>
2017-11-02 16:06:35 -0500

    Elements del BookFlight

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # testng-newtours-eclipse
 Automatización de pruebas funcionales con Test Ng y Eclipse.
 Temporalmente drivers aquí: https://drive.google.com/a/uqvirtual.edu.co/file/d/0B_ySWmGf20L-ZzEyVXNDS1BkMHM/view?usp=sharing
+
+Ya no son necesarios los Jars de Selenium porque el proyecto trabajo con Maven.
--- src/test/BookFlightTest.java
+++ src/test/BookFlightTest.java
@@ -21,6 +21,8 @@ public class BookFlightTest extends TestBase {
 	protected FindFlightPage flightP;
 	protected LoginPage login;
 	protected BookFlightPage bookFlightP;
+	protected FindFlightTest findFlight;
+	protected SelectFlightTest selectflight;
 
 	// Valores para seleccionar las opciones en la página
 
@@ -49,6 +51,15 @@ public class BookFlightTest extends TestBase {
 				"SIGN-OFF"))) {
 			Assert.fail("El link no contiene el texto esperado SIGN-OFF");
 		}
+
+		// Luego relleno el formulario de Find Flight
+		findFlight = new FindFlightTest();
+		findFlight.seleccionarOpcionesVuelo();
+
+		// Luego selecciono depart y return en select flight
+		selectflight = new SelectFlightTest();
+		selectflight.seleccionarDepartAndReturn();
+
 	}// Fin método validarTextLinks
 
 	@Test(priority = 1)
@@ -57,10 +68,119 @@ public class BookFlightTest extends TestBase {
 		bookFlightP = new BookFlightPage(driver, pageTitleBookFlight);
 
 		// verifico los elementos en la página
-		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP.getInputFirstName0()))) {
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputFirstName0()))) {
 			Assert.fail("No se encontró el input first name 0");
 		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputLastName0()))) {
+			Assert.fail("No se encontró el input last name 0");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectMeal0()))) {
+			Assert.fail("No se encontró el select meal 0");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputFirstName1()))) {
+			Assert.fail("No se encontró el input first name 1");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputLastName1()))) {
+			Assert.fail("No se encontró el input last name 1");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectMeal1()))) {
+			Assert.fail("No se encontró el select meal 1");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectCreditCard()))) {
+			Assert.fail("No se encontró el seelect credit card");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputCreditNumber()))) {
+			Assert.fail("No se encontró el input credit number");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectCcExpM()))) {
+			Assert.fail("No se encontró el select expirate month cc");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectCcExpY()))) {
+			Assert.fail("No se encontró el select expirate year cc");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputCcFirstName()))) {
+			Assert.fail("No se encontró el input first name cc");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputCcMiddleName()))) {
+			Assert.fail("No se encontró el input middle name cc");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputCcLastName()))) {
+			Assert.fail("No se encontró el input last name cc");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getCheckTicketLess()))) {
+			Assert.fail("No se encontró el check ticket less");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputBillAddress1()))) {
+			Assert.fail("No se encontró el input bill address 1");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputBillAddress2()))) {
+			Assert.fail("No se encontró el input bill address 2");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputBillCity()))) {
+			Assert.fail("No se encontró el input bill city");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputBillState()))) {
+			Assert.fail("No se encontró el input bill state");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputBillZip()))) {
+			Assert.fail("No se encontró el input bill postal code");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectBillCountry()))) {
+			Assert.fail("No se encontró el select bill country");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getCheckBillingAddress()))) {
+			Assert.fail("No se encontró el check billing address");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputDelAddress1()))) {
+			Assert.fail("No se encontró el input del address 1");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputDelAddress2()))) {
+			Assert.fail("No se encontró el input del address 2");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputDelCity()))) {
+			Assert.fail("No se encontró el input del city");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputDelState()))) {
+			Assert.fail("No se encontró el input del state");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getInputDelZip()))) {
+			Assert.fail("No se encontró el input del postal code");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getSelectDelCountry()))) {
+			Assert.fail("No se encontró el select del country");
+		}
+		if (!(bookFlightP.isElementPresentAndDisplay(bookFlightP
+				.getButtonBuyFlights()))) {
+			Assert.fail("No se encontró el button buy flights");
+		}
 
-	}
+	}// Fin método verificarElementosBookFlight
 
-}
+}// Fin clase BookFlightTest
